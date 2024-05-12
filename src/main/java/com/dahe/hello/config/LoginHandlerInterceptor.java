fa95aa4f657129e1890ea02335b66b0439cf2297
dahezhiquan <3390205563@qq.com>
2024-05-12 23:31:18 +0800

    feat: 新增登陆拦截器

---
+++ src/main/java/com/dahe/hello/config/LoginHandlerInterceptor.java
@@ -0,0 +1,23 @@
+package com.dahe.hello.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登陆拦截器
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object session = request.getSession().getAttribute("LoginUser");
+        if (session == null) {
+            request.setAttribute("msg", "请先登录");
+            request.getRequestDispatcher("/login").forward(request, response);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
