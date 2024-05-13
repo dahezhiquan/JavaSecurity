a0454e131bed2fa7018b32d7164188ac621bb6bf
dahezhiquan <3390205563@qq.com>
2024-05-13 15:51:41 +0800

    feat: 新增Swagger未授权漏洞

---
+++ src/main/java/com/dahe/hello/Swagger2.java
@@ -0,0 +1,46 @@
+package com.dahe.hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    // swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                // 为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.dahe.hello.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    // 构建 api 文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger2 RESTful API")
+                //创建人
+                .contact(new Contact("dahezhiquan", "https://www.github.com/dahezhiquan", "3390205563@qq.com"))
+                .version("1.10")
+                .description("JavaSecurity API")
+                .build();
+    }
+
+
+}
